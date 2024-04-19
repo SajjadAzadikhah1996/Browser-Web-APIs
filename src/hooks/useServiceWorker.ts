0c145dc9b7664274af69f8edaaa4c40c5dbb683e
Sajjad1996 <sajjad75926@gmail.com>
2024-04-19 20:10:59 +0330

    Service worker instance methods.

--- src/hooks/useServiceWorker.ts
+++ src/hooks/useServiceWorker.ts
@@ -12,25 +12,14 @@ export default function useServiceWorker() {
                         { scope: '/' }
                     );
 
-                    // controller and ready are instance properties of service worker.
+                    navigator.serviceWorker.getRegistration().then((registration) => {
+                        if (registration)
+                            console.log('ServiceWorkerRegistration found.', registration);
+                    });
 
-                    /*
-                   The controller property returns null if the request is a force refresh (Shift + refresh)
-                   or if there is no active worker.
-                   * */
-                    if (navigator.serviceWorker.controller)
-                        console.log('This page is currently controlled by:', navigator.serviceWorker.controller);
-                    else
-                        console.log("This page is not currently controlled by a service worker.");
-
-
-                    // ready property provides a way of delaying code execution until a service worker is active.
-                    navigator.serviceWorker.ready.then((registration) => {
-                        console.log('A service worker is active: ', registration.active);
-                        /*
-                         At this point, you can call methods that require an active service worker,
-                         like registration.pushManager.subscribe()
-                        * */
+                    navigator.serviceWorker.getRegistrations().then((registrations) => {
+                        if (registrations)
+                            console.log('ServiceWorkerRegistrations found.', registrations);
                     });
 
                     setServiceWorker(_serviceWorker);
