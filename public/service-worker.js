270528c7b9d9971a36e522df435c0452326b551d
Sajjad1996 <sajjad75926@gmail.com>
2024-04-19 17:04:44 +0330

    Check if a page is controlled by a service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,6 @@
-// SERVICE WORKER EVENTS
-
-
-// The installation event is fired when an installation is successfully completed.
-
-/*
-* The first event a service worker gets is install. It's triggered as soon as the worker executes,  and it's only called
-    once per service worker(refresh the page, you will see this not calling anymore until unregister service worker)
-* */
-
-/*
-* When you register a new version of service worker(by changing the source code of this file),
-    refreshing the page fires it again (because browser it changed and browser install it again), but
-    does not active it(because we have already an active service worker.)(waiting mode)
-    actually we can have another installed service worker, but we just have only one active service worker.
-* */
-
 self.addEventListener('install', (eve) => {
     console.log('Event: Service worker installed.')
 })
-
-/*
-* This fires once at first time because after installing service worker it must be active.
-* This fires once the old service worker is gone, and your new service worker is able to control clients.
-* */
 
 self.addEventListener('activate', (eve) => {
     console.log('Event: Service worker activated.')
--- src/hooks/useServiceWorker.ts
+++ src/hooks/useServiceWorker.ts
@@ -3,7 +3,6 @@ import {useEffect, useState} from "react";
 export default function useServiceWorker() {
     const [serviceWorker, setServiceWorker] = useState<ServiceWorkerRegistration>();
 
-    // useEffect just runs one time at first time. open console and refresh the page to see update cycle.
     useEffect(() => {
         const registerServiceWorker = async () => {
             if ("serviceWorker" in navigator) {
@@ -12,35 +11,18 @@ export default function useServiceWorker() {
                         './service-worker.js',
                         { scope: '/' }
                     );
-                    // SERVICE WORKER NOTIFICATIONS
-                    if (_serviceWorker.installing) {
-                        /*
-                        * This block runs once you register service worker (we registered it in ServiceWorkerProvider.tsx file )
-                           after that when you refresh the page this block not running anymore.
-                        * */
-                        console.log("Notification: Service worker is installing");
-                    } else if (_serviceWorker.waiting) {
-                        /*
-                        * After it's successfully installed, the updated service worker delays activating until
-                           the existing service worker is no longer controlling clients. This state is called "waiting",
-                           and it's how the browser ensures that only one version of your service worker is running at a time.
-                        * Once all pages controlled by the old version of the service worker have closed, it's safe to
-                           retire the old version, and the newly installed service worker receives an activate event.
-                        * Call skipWaiting() to ask to be activated immediately without waiting for open pages to be closed.
-                        * To override this default behavior and adopt open pages, a service worker can call clients.claim().
-                        * */
-                        console.log("Notification: Service Worker is installed and active, but a new version of Service " +
-                            "Worker is available. (waiting mode)");
-                    } else if (_serviceWorker.active) {
-                        /*
-                        * This block runs once the old service worker is gone, and your new service worker(or first service worker)
-                           is able to control clients.
-                        * */
-                        console.log("Notification: Service worker is active");
-                    }
+                    /*
+                    The controller property returns null if the request is a force refresh (Shift + refresh)
+                    or if there is no active worker.
+                    * */
+                    if (navigator.serviceWorker.controller)
+                        console.log('This page is currently controlled by:', navigator.serviceWorker.controller);
+                    else
+                        console.log("This page is not currently controlled by a service worker.");
+
                     setServiceWorker(_serviceWorker);
                 } catch (err) {
-                    console.log(`Notification: Registration failed with ${err} `);
+                    console.log(`Registration failed with ${err} `);
                 }
             } else
                 console.log('Service worker is not supported.');
